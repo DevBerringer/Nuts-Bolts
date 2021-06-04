230af1da62645ece62d01d5e8183c85fbd3ec79f
BlakeBerringer <blakeberringer@gmail.com>
2021-06-03 22:31:13 -0500

    add entity models

---
+++ NutsAndBolts.Data/Modals/ProductInventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutsAndBolts.Data.Modals
+{
+    public class ProductInventory
+    {
+        public int Id { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public int QuantityOnHand { get; set; }
+        public int IdealQuantity { get; set; }
+
+        public Product Product { get; set; }
+
+    }
+}
---
+++ NutsAndBolts.Data/Modals/ProductInventorySnapshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutsAndBolts.Data.Modals
+{
+    public class ProductInventorySnapshot
+    {
+        public int Id { get; set; }
+        public DateTime SnapShotTime { get; set; }
+        public int QuantityOnHand { get; set; }
+        public Product Product { get; set; }
+    }
+}
